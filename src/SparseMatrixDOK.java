dfe60f980c9ccddf8ddb701e495c75ebbe1b89ee
Vasilis K <dit18103@uop.gr>
2021-06-08 21:11:00 +0300

    final commit all ready

--- src/SparseMatrixDOK.java
+++ src/SparseMatrixDOK.java
@@ -1,30 +1,77 @@
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class SparseMatrixDOK implements SparseMatrix {
-
+    private int r , c;
+  
+    private class ElementPos implements Comparable<ElementPos>{
+        private int row,col;
+        public ElementPos(int row,int col){
+            this.row = row;
+            this.col = col;
+        }
+        @Override
+        public int compareTo(SparseMatrixDOK.ElementPos o) {
+            if(this.row>o.row){
+                return 1;
+            }
+            else if(this.row<o.row){
+                return -1;
+            }
+            else{
+                if(this.col>o.col){
+                    return 1;
+                }
+                else if(this.col<o.col){
+                    return -1;
+                }
+            }
+            return 0;
+        }
+    }
+    private TreeMap<ElementPos,Double> map;
     public SparseMatrixDOK(int r, int c){
-
+        map = new TreeMap<ElementPos,Double>();
+        this.r = r;
+        this.c = c;
     }
     public int rowCount(){
-        return 0;
+        return this.r;
     }
     public int colCount(){
-        return 0;
+        return this.c;
     }
     public double get(int r, int c){
-        return 0;
+        Double check = map.get(new ElementPos(r, c));
+        if(check!=null){
+            return check;
+        }
+        return 0.0;
     }
     public void set(int r, int c, double element){
-
+       this.map.put(new ElementPos(r, c), element);  
     }
     public void zero(int r, int c){
-
+        this.map.remove(new ElementPos(r, c));
     }
     public void clear(){
-
+        this.map.clear();
     }
     public boolean isEmpty(){
-        return true;
+        return this.map.isEmpty();
     }
     public String toString(){
-        return "DOK";
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        Set<ElementPos> keys = map.keySet();
+        for (ElementPos k : keys){
+            Double val = map.get(k);
+            s.append("("+k.row+", "+ k.col+": "+ val+")");
+        }
+        s.append("]");
+        return s.toString();
     }
 }
